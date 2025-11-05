c34b1f986063f6116ab76e18f5f7644a14a1fa10
Ralph <Hiimralphhh@gmail.com>
2025-11-06 01:09:14 +0800

    Personal Info Display

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    
+    $name = "Ralph";
+    $age = 22;
+    $color = "Black";
+    $food = "Pizza";
+
+    echo "My name is $name, I am $age years old, my favorite color is $color, and I love $food.";
+
+
+
+?>
