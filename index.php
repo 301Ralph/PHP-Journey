4539909bcc86d9fc3c2b04f4344bb2155152b210
Ralph <Hiimralphhh@gmail.com>
2025-11-06 01:15:18 +0800

    Temperature Converter

--- index.php
+++ index.php
@@ -1,19 +1,9 @@
 <?php
     
-    $a = 15;
-    $b = 4;
+    $temperature = 25; 
+    $fahrenheit = ($temperature * 9/5) + 32;
 
-    $sum = $a + $b;
-    $difference = $a - $b;
-    $product = $a * $b;
-    $quotient = $a / $b;
-    $modulus = $a % $b;
-    echo "Sum: $sum <br>";
-    echo "Difference: $difference <br>";
-    echo "Product: $product <br>";
-    echo "Quotient: $quotient <br>";
-    echo "Modulus: $modulus <br>";
-
+    echo "{$temperature}°C is equal to {$fahrenheit}°F.";
 
 
 ?>
