0b29ed4b7bf7fc0b8ff50dc37380162c26f29515
Ralph <Hiimralphhh@gmail.com>
2025-11-06 01:23:58 +0800

    Variable Swapping

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
     
-    $hourly_rate = 150;
-    $hours_worked_per_week = 40;
-    $weekly_salary = $hourly_rate * $hours_worked_per_week;
-    $monthly_salary = $weekly_salary * 4; 
-    echo "Hourly rate: $hourly_rate <br>";
-    echo "Weekly salary: $weekly_salary <br>";
-    echo "Monthly salary: $monthly_salary <br>";
+    $a = 10;
+    $b = 20;
+    $a = $a + $b; // 30
+    $b = $a - $b; // 10
+    $a = $a - $b; // 20
+    echo "a: $a, b: $b";
+    
 ?>
