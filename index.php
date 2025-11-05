622130ecebe9762ca3fe6274f41193d39566d0aa
Ralph <Hiimralphhh@gmail.com>
2025-11-06 01:17:45 +0800

    Simple Salary Calculator

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
     
-    $temperature = 25; 
-    $fahrenheit = ($temperature * 9/5) + 32;
-
-    echo "{$temperature}°C is equal to {$fahrenheit}°F.";
-
-
+    $hourly_rate = 150;
+    $hours_worked_per_week = 40;
+    $weekly_salary = $hourly_rate * $hours_worked_per_week;
+    $monthly_salary = $weekly_salary * 4; 
+    echo "Hourly rate: $hourly_rate <br>";
+    echo "Weekly salary: $weekly_salary <br>";
+    echo "Monthly salary: $monthly_salary <br>";
 ?>
