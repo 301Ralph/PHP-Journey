826f9e2c960459b1493c4a75cc501d79fc391f68
Ralph <Hiimralphhh@gmail.com>
2025-11-08 02:00:27 +0800

    Learning  and

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <?php
-    
-    $fname = "Ralph";
-    $mname = "Aian";
-    $lname = "Escote";
-    $name = "$fname $mname $lname";
-    $name = strtoupper($name);
-    echo "Hi my name is $name";
-    
+    $food = "Pizza";
+    $price = 10.00;
+    $qty = $_POST["qty"];
+    $total = $price * $qty;
+
+    echo "Food Item: $food <br>";
+    echo "Total: $total <br>";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label>Quantity:</label>
+        <input type="text" name="qty"><br>
+        <input type="submit" value="Total">
+    </form>
+</body>
+</html>
+
